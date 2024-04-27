a3fd425d89fba75139947de6806ca6ed60bca621
Matt Bremmers <bremmdev@gmail.com>
2024-04-27 09:41:46 +0200

    add reminderTime check to form validation

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,20 +2,25 @@ import z from "zod";
 
 export const REMINDER_TIMES = ["1", "5", "10", "15", "30", "60"];
 
-export const formSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  description: z.string().min(1, "Description is required"),
-  expirationTime: z.coerce
-    .number()
-    .min(2)
-    .max(7200, "Expiration time must be between 2 and 7200 seconds")
-    .transform((val) => val.toString()),
-  reminderTime: z
-    .string()
-    .refine((val) => REMINDER_TIMES.includes(val), {
+export const formSchema = z
+  .object({
+    name: z.string().min(1, "Name is required"),
+    description: z.string().min(1, "Description is required"),
+    expirationTime: z.coerce
+      .number()
+      .min(2)
+      .max(7200, "Expiration time must be between 2 and 7200 seconds")
+      .transform((val) => val.toString()),
+    reminderTime: z.string().refine((val) => REMINDER_TIMES.includes(val), {
       message: "Reminder time not valid",
     }),
-});
+  })
+  .refine(
+    (data) => {
+      return parseInt(data.expirationTime) > parseInt(data.reminderTime);
+    },
+    { message: "Reminder time must be less than expiration time", path: ["reminderTime"]}
+  );
 
 export type FormSchema = z.infer<typeof formSchema>;
 
